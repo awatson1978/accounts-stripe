872e49142c6103885a4e9f08f3a2e430c3ae869c
Abigail Watson <awatson1978@gmail.com>
2013-08-22 23:29:47 -0400

    initial commit

---
+++ package.js
@@ -0,0 +1,16 @@
+Package.describe({
+    summary: "Accounts service for Stripe accounts"
+});
+
+Package.on_use(function(api) {
+    api.use('accounts-base', ['client', 'server']);
+    api.imply('accounts-base', ['client', 'server']);
+
+    api.use('accounts-oauth', ['client', 'server']);
+
+    api.add_files(['stripe_login_button.css'], 'client');
+
+    api.add_files('stripe_common.js', ['client', 'server']);
+    api.add_files('stripe_server.js', 'server');
+    api.add_files('stripe_client.js', 'client');
+});
---
+++ stripe_common.js
@@ -0,0 +1,3 @@
+if (!Accounts.stripe) {
+    Accounts.stripe = {};
+}
